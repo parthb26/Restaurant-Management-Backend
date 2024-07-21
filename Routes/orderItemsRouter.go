124651d0dccb550616eb312538a36d098145a5fb
Parth Bilgaiyan <166209744+parthb26@users.noreply.github.com>
2024-07-22 03:29:58 +0530

    Create orderItemsRouter.go

---
+++ Routes/orderItemsRouter.go
@@ -0,0 +1,17 @@
+package routes
+
+import (
+	controller "restaurant-management/controllers"
+
+	"github.com/gin-gonic/gin"
+)
+
+func OrderItemRoutes(incomingRoutes *gin.Engine) {
+	incomingRoutes.GET("/orderItems", controller.GetOrderItems())
+	incomingRoutes.GET("/orderItems/:orderItems_id", controller.GetOrderItem())
+
+	// here we get all the order items inside the order id
+	incomingRoutes.GET("/orderItems-order/:order_id", controller.GetOrderItemsByOrder())
+	incomingRoutes.POST("/orderItems", controller.CreateOrderItem())
+	incomingRoutes.PATCH("/orderItems/:orderItems_id", controller.UpdateOrderItem())
+}
