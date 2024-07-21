f94054cd76868be1ce7e6c2bc39eafa514824136
Parth Bilgaiyan <166209744+parthb26@users.noreply.github.com>
2024-07-22 03:22:35 +0530

    Create databaseConnection.go

---
+++ Database/databaseConnection.go
@@ -0,0 +1,40 @@
+package database
+
+import (
+	"context" // to pass some context with timeout
+	"fmt"     // to print out stuff
+	"log"     // to log out errors
+	"time"    // to give time out
+
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+func DBinstance() *mongo.Client {
+	MongoDb := "mongodb://localhost/"
+	fmt.Print(MongoDb)
+
+	client, err := mongo.NewClient(options.Client().ApplyURI(MongoDb))
+	if err != nil {
+		log.Fatal(err)
+	}
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+
+	defer cancel()
+
+	err = client.Connect(ctx)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("connected to mongodb")
+	return client
+}
+
+var Client *mongo.Client = DBinstance()
+
+func OpenCollection(client *mongo.Client, collectionName string) *mongo.Collection {
+	var collection *mongo.Collection = client.Database("restaurant").Collection(collectionName)
+
+	return collection
+}
